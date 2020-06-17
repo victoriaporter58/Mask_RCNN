c05a0c3fb8934e5363f3e9e93e06de1a12d96397
Victoria Porter <44816029+victoriaporter58@users.noreply.github.com>
2020-06-17 13:14:28 +0100

    Update regions.py

--- samples/regions/regions.py
+++ samples/regions/regions.py
@@ -197,7 +197,8 @@ def train(model):
     dataset_val.load_custom(args.dataset, "val")
     dataset_val.prepare()
 
-    augmentation = imgaug.augmenters.Affine(rotate=(-45, 45))
+    augmentation = imgaug.Sometimes(5/6,aug.OneOf([imgaug.augmenters.Fliplr(1),imgaug.augmenters.Affine(rotate=(-45, 45)),imgaug.augmenters.Affine(scale=(0.9, 1.1))]))
+
     print("Augmentation: ", augmentation)
 
     # *** This training schedule is an example. Update to your needs ***
