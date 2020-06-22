8dbc557ab804fc640575efab50fba84155ad0df4
Victoria Porter <44816029+victoriaporter58@users.noreply.github.com>
2020-06-22 18:19:19 +0100

    Update regions.py

--- samples/regions/regions.py
+++ samples/regions/regions.py
@@ -74,7 +74,7 @@ class CustomConfig(Config):
     NUM_CLASSES = 1 + 6  # Background + balloon
 
     # Number of training steps per epoch
-    STEPS_PER_EPOCH = 900 #100
+    STEPS_PER_EPOCH = 100 #100
 
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.95
