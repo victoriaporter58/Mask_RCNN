06a918fbf417ab5b4ec4c39d2be1717d45d8dd4c
Victoria Porter <44816029+victoriaporter58@users.noreply.github.com>
2020-07-23 10:19:31 +0100

    Update regions.py

--- samples/regions/regions.py
+++ samples/regions/regions.py
@@ -204,15 +204,18 @@ def train(model):
     augmentation = imgaug.augmenters.Sometimes(9/10, imgaug.augmenters.OneOf([
            imgaug.augmenters.Flipud(1.0),
            imgaug.augmenters.Affine(rotate=(-135, 135)),
-           imgaug.augmenters.Multiply((0.5, 1.5), per_channel=0.5),#brightness and colour channel adjustment
-           imgaug.augmenters.Affine(shear=(-16, 16)),
+           imgaug.augmenters.Affine(translate_px=(-50, 50)),
            imgaug.augmenters.Affine(scale=(0.5,1.5)),
+           imgaug.augmenters.Affine(shear=(-16, 16)),
+           imgaug.augmenters.Multiply((0.5, 1.5), per_channel=0.5),#brightness and colour channel adjustment
            imgaug.augmenters.Grayscale(alpha=(0.0, 1.0)),
-           imgaug.augmenters.AddToHueAndSaturation((-50, 50), per_channel=True),
            imgaug.augmenters.ContrastNormalization((0.75, 1.5)),#contrast
-           imgaug.augmenters.GaussianBlur(sigma=(0.0, 0.5)),
-           imgaug.augmenters.Affine(translate_px=(-40, 40))
+           imgaug.augmenters.GaussianBlur(sigma=(0.0, 0.5))
            
+           
+           
+           
+           #imgaug.augmenters.AddToHueAndSaturation((-50, 50), per_channel=True),
            #imgaug.augmenters.AdditiveGaussianNoise(scale=0.2*255, per_channel=True),#noise created by greyscale & colour pixel replacements
            #imgaug.augmenters.AdditiveLaplaceNoise(scale=0.2*255, per_channel=True),#like gaussian noise except more likely to use high/low values
            #imgaug.augmenters.Crop(percent=(0, 0.1)),
@@ -242,7 +245,7 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=30,
+                epochs=130,
                 layers='heads',
                 augmentation = augmentation)
                 
