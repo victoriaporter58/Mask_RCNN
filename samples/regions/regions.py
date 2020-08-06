76bfee5c3009f8a7c5afc8bc8e398691c706da21
Victoria Porter <44816029+victoriaporter58@users.noreply.github.com>
2020-08-06 11:25:00 +0100

    Update regions.py

--- samples/regions/regions.py
+++ samples/regions/regions.py
@@ -236,7 +236,7 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=150,
+                epochs=500,
                 layers='heads',
                 augmentation = augmentation)
                 
